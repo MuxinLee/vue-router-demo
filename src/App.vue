aa888e4f89a70c2e57d0c59d7681b09cb51f428c
datura_lj <datura_lj@163.com>
2017-09-17 12:12:57 +0800

    vue-rputer入门

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h1>我是初始化页面</h1>  
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+*{
+  margin: 0;
+  padding: 0;
+  }
+li {
+  list-style: none;
+  display: inline-block;
+}
+</style>
